608cb4e91daa39d39af764596c55cc4e0066969d
Matt Lewis <matt@clickup.com>
2025-01-23 14:30:30 +0000

    perf(platform-browser): disable styles of removed components instead of removing

--- packages/platform-browser/src/dom/dom_renderer.ts
+++ packages/platform-browser/src/dom/dom_renderer.ts
@@ -55,7 +55,7 @@ const REMOVE_STYLES_ON_COMPONENT_DESTROY_DEFAULT = true;
 
 /**
  * A DI token that indicates whether styles
- * of destroyed components should be removed from DOM.
+ * of destroyed components should be disabled.
  *
  * By default, the value is set to `true`.
  * @publicApi
@@ -93,7 +93,7 @@ export class DomRendererFactory2 implements RendererFactory2, OnDestroy {
     private readonly eventManager: EventManager,
     private readonly sharedStylesHost: SharedStylesHost,
     @Inject(APP_ID) private readonly appId: string,
-    @Inject(REMOVE_STYLES_ON_COMPONENT_DESTROY) private removeStylesOnCompDestroy: boolean,
+    @Inject(REMOVE_STYLES_ON_COMPONENT_DESTROY) private disableStylesOnCompDestroy: boolean,
     @Inject(DOCUMENT) private readonly doc: Document,
     @Inject(PLATFORM_ID) readonly platformId: Object,
     readonly ngZone: NgZone,
@@ -143,7 +143,7 @@ export class DomRendererFactory2 implements RendererFactory2, OnDestroy {
       const ngZone = this.ngZone;
       const eventManager = this.eventManager;
       const sharedStylesHost = this.sharedStylesHost;
-      const removeStylesOnCompDestroy = this.removeStylesOnCompDestroy;
+      const disableStylesOnCompDestroy = this.disableStylesOnCompDestroy;
       const platformIsServer = this.platformIsServer;
 
       switch (type.encapsulation) {
@@ -153,7 +153,7 @@ export class DomRendererFactory2 implements RendererFactory2, OnDestroy {
             sharedStylesHost,
             type,
             this.appId,
-            removeStylesOnCompDestroy,
+            disableStylesOnCompDestroy,
             doc,
             ngZone,
             platformIsServer,
@@ -177,7 +177,7 @@ export class DomRendererFactory2 implements RendererFactory2, OnDestroy {
             eventManager,
             sharedStylesHost,
             type,
-            removeStylesOnCompDestroy,
+            disableStylesOnCompDestroy,
             doc,
             ngZone,
             platformIsServer,
@@ -505,14 +505,14 @@ class ShadowDomRenderer extends DefaultDomRenderer2 {
 }
 
 class NoneEncapsulationDomRenderer extends DefaultDomRenderer2 {
-  private readonly styles: string[];
+  protected styles: string[];
   private readonly styleUrls?: string[];
 
   constructor(
     eventManager: EventManager,
     private readonly sharedStylesHost: SharedStylesHost,
     component: RendererType2,
-    private removeStylesOnCompDestroy: boolean,
+    private disableStylesOnCompDestroy: boolean,
     doc: Document,
     ngZone: NgZone,
     platformIsServer: boolean,
@@ -520,7 +520,7 @@ class NoneEncapsulationDomRenderer extends DefaultDomRenderer2 {
     compId?: string,
   ) {
     super(eventManager, doc, ngZone, platformIsServer, tracingService);
-    this.styles = compId ? shimStylesContent(compId, component.styles) : component.styles;
+    this.styles = component.styles;
     this.styleUrls = component.getExternalStyles?.(compId);
   }
 
@@ -529,11 +529,9 @@ class NoneEncapsulationDomRenderer extends DefaultDomRenderer2 {
   }
 
   override destroy(): void {
-    if (!this.removeStylesOnCompDestroy) {
-      return;
+    if (this.disableStylesOnCompDestroy) {
+      this.sharedStylesHost.disableStyles(this.styles, this.styleUrls);
     }
-
-    this.sharedStylesHost.removeStyles(this.styles, this.styleUrls);
   }
 }
 
@@ -546,7 +544,7 @@ class EmulatedEncapsulationDomRenderer2 extends NoneEncapsulationDomRenderer {
     sharedStylesHost: SharedStylesHost,
     component: RendererType2,
     appId: string,
-    removeStylesOnCompDestroy: boolean,
+    disableStylesOnCompDestroy: boolean,
     doc: Document,
     ngZone: NgZone,
     platformIsServer: boolean,
@@ -557,13 +555,14 @@ class EmulatedEncapsulationDomRenderer2 extends NoneEncapsulationDomRenderer {
       eventManager,
       sharedStylesHost,
       component,
-      removeStylesOnCompDestroy,
+      disableStylesOnCompDestroy,
       doc,
       ngZone,
       platformIsServer,
       tracingService,
       compId,
     );
+    this.styles = shimStylesContent(compId, component.styles);
     this.contentAttr = shimContentAttribute(compId);
     this.hostAttr = shimHostAttribute(compId);
   }
